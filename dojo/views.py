2c70410bd2798a5adb579adbf76ed418f38b80ba
Harold <harold.a.sy@gmail.com>
2026-04-17 10:27:18 -0700

    added dojo/views.py

--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .questions import get_random_question
 
-# Create your views here.
+def dojo_view(request):
+    question = get_random_question()
+    result = None
+
+    if request.method == "POST":
+        user_answer = request.POST.get("answer")
+        correct_answer = request.POST.get("correct_answer")
+
+        if user_answer == correct_answer:
+            result = "Correct!"
+        else:
+            result = "Wrong!"
+
+    return render(request, "dojo.html", {
+        "question": question,
+        "result": result
+    })
+
+
